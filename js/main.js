0699040286d60f4dc4010c6d00e53ca24c91d45b
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-17 19:15:21 -0400

    added missing <>

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 grains.forEach(function (item) {
   var $ul = $('ul');
-  var $li = $('li');
-  var $img = $('img');
-  var $p = $('p');
-  var $h2 = $('h2');
+  var $li = $('<li>');
+  var $img = $('<img>');
+  var $p = $('<p>');
+  var $h2 = $('<h2>');
 
   $ul.append($li);
   $img.attr('src', 'images/' + item.img);
